da5b6bac6abba54e8f981256cc37571a7cbd4cea
asm2212 <asmareadmasu0@gmail.com>
2024-06-18 19:43:24 +0300

    ui is ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,217 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:music_player_app_ui/screens/song_details_screen.dart';
+import 'package:music_player_app_ui/songs_provider.dart';
+import 'components/curved_card.dart';
+import 'components/song_item.dart';
+import 'constants.dart';
+import 'model/song.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MusicApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MusicApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: kPrimaryColor,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomeScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomeScreenState extends State<HomeScreen> {
+  int currentBottomTab = 0;
+  int currentSongPosition = -1;       //Initially no song selected
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: kPrimaryColor,
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        elevation: 0,
+        title: Text(
+          "Playlist",
+          style: TextStyle(
+            fontSize: 16,
+          ),
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView.builder(
+        physics: BouncingScrollPhysics(),
+        itemBuilder: (_, position) {
+          Song song = SongsProvider.songs[position % SongsProvider.songs.length];   //Handling out of bounds
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                currentSongPosition = position;
+              });
+            },
+            onDoubleTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (_) => SongDetailsScreen(song: song,)));
+            },
+            child: SongRow(
+              songName: song.songName,
+              album: song.album,
+              artistCoverPic: song.artistCoverPic,
+              duration: song.duration,
+              rating: song.rating,
+              isPlaying: currentSongPosition == position,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          );
+        },
+        itemCount: SongsProvider.songs.length * 2,      //just to populate more data
+      ),
+      bottomNavigationBar: _buildBottomBar(),
+    );
+  }
+
+  Widget _buildBottomBar() {
+
+    Widget _buildCurrentSongSection() {
+
+      if (currentSongPosition == -1)
+        return Container();
+
+      Song song = SongsProvider.songs[currentSongPosition % SongsProvider.songs.length];
+
+      return Padding(
+        padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 4,),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(12), bottomRight: Radius.circular(12),),
+                  child: Image.network(
+                    song.artistCoverPic,
+                    height: 45,
+                    width: 45,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(song.songName,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                        SizedBox(height: 6,),
+                        Text(song.album,
+                          style: TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                        SizedBox(height: 6,),
+                        RatingBar(
+                          onRatingUpdate: (d){},
+                          itemSize: 12,
+                          unratedColor: Colors.grey,
+                          glowColor: Colors.white,
+                          allowHalfRating: true,
+                          initialRating: song.rating,
+                          itemCount: 5,
+                          itemBuilder: (context, _) => Icon(Icons.star,),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.skip_previous,),
+                    SizedBox(width: 4,),
+                    CurvedCard(
+                      child: Container(
+                        padding: EdgeInsets.all(8),
+                        child: Icon(Icons.pause,),
+                      ),
+                      topLeft: 12,
+                      bottomRight: 12,
+                      color: Color(0xff342D35),
+                    ),
+                    SizedBox(width: 4,),
+                    Icon(Icons.skip_next,),
+                  ],
+                )
+              ],
+            ),
+            Slider(value: 30,
+              onChanged: (d){},
+              activeColor: Colors.white,
+              inactiveColor: Colors.grey,
+              min: 0,
+              max: 100,
             ),
           ],
         ),
+      );
+    }
+
+
+    return CurvedCard(
+      color: Color(0xff574954),
+      topLeft: kBottomNavCornerRadius,
+      bottomRight: kBottomNavCornerRadius,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _buildCurrentSongSection(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              IconButton(
+              icon: Icon(
+                Icons.queue_music,
+                color: currentBottomTab == 0 ? Colors.white : Colors.grey[400],
+              ),
+              padding: EdgeInsets.all(8),
+              onPressed: () {
+                setState(() {
+                  currentBottomTab = 0;
+                });
+              }),
+              IconButton(
+              icon: Icon(
+                Icons.search,
+                color: currentBottomTab == 1 ? Colors.white : Colors.grey[400],
+              ),
+              padding: EdgeInsets.all(8),
+              onPressed: () {
+                setState(() {
+                  currentBottomTab = 1;
+                });
+              }),
+              IconButton(
+              icon: Icon(
+                Icons.person_outline,
+                color: currentBottomTab == 2 ? Colors.white : Colors.grey[400],
+              ),
+              padding: EdgeInsets.all(8),
+              onPressed: () {
+                setState(() {
+                  currentBottomTab = 2;
+                });
+              }),
+            ],
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
